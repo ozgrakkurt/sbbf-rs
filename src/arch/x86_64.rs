cf97924a9b99a6ecb0970db8f06366512c82bf6b
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2023-06-23 20:15:35 +0600

    smoke_test_sse

--- src/arch/x86_64.rs
+++ src/arch/x86_64.rs
@@ -125,34 +125,34 @@ impl FilterImpl for SseFilter {
 
 #[cfg(test)]
 mod test {
-    use crate::ALIGNMENT;
-    use std::alloc::{alloc_zeroed, dealloc, Layout};
+    #[cfg(target_feature = "sse4.1")]
+    #[test]
+    fn smoke_test_sse() {
+        use crate::ALIGNMENT;
+        use std::alloc::{alloc_zeroed, dealloc, Layout};
 
-    struct Buf {
-        ptr: *mut u8,
-        layout: Layout,
-    }
-
-    impl Buf {
-        fn new(len: usize) -> Self {
-            let layout = Layout::from_size_align(len, ALIGNMENT).unwrap();
-            let ptr = unsafe { alloc_zeroed(layout) };
-
-            Self { layout, ptr }
+        struct Buf {
+            ptr: *mut u8,
+            layout: Layout,
         }
-    }
 
-    impl Drop for Buf {
-        fn drop(&mut self) {
-            unsafe {
-                dealloc(self.ptr, self.layout);
+        impl Buf {
+            fn new(len: usize) -> Self {
+                let layout = Layout::from_size_align(len, ALIGNMENT).unwrap();
+                let ptr = unsafe { alloc_zeroed(layout) };
+
+                Self { layout, ptr }
             }
         }
-    }
 
-    #[test]
-    #[cfg(target_feature = "sse4.1")]
-    fn smoke_test_sse() {
+        impl Drop for Buf {
+            fn drop(&mut self) {
+                unsafe {
+                    dealloc(self.ptr, self.layout);
+                }
+            }
+        }
+
         let buf = Buf::new(64);
 
         assert!(!SseFilter.insert(buf.ptr, 2, 69));
