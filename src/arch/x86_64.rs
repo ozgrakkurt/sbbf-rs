9db367eda80e5f4c0f6ce7ad29da3007c551c9a5
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2023-06-23 20:21:55 +0600

    fix import

--- src/arch/x86_64.rs
+++ src/arch/x86_64.rs
@@ -125,33 +125,34 @@ impl FilterImpl for SseFilter {
 
 #[cfg(test)]
 mod test {
+    use super::*;
+    use crate::ALIGNMENT;
+    use std::alloc::{alloc_zeroed, dealloc, Layout};
+
+    struct Buf {
+        ptr: *mut u8,
+        layout: Layout,
+    }
+
+    impl Buf {
+        fn new(len: usize) -> Self {
+            let layout = Layout::from_size_align(len, ALIGNMENT).unwrap();
+            let ptr = unsafe { alloc_zeroed(layout) };
+
+            Self { layout, ptr }
+        }
+    }
+
+    impl Drop for Buf {
+        fn drop(&mut self) {
+            unsafe {
+                dealloc(self.ptr, self.layout);
+            }
+        }
+    }
+
     #[test]
     fn smoke_test_sse() {
-        use crate::ALIGNMENT;
-        use std::alloc::{alloc_zeroed, dealloc, Layout};
-
-        struct Buf {
-            ptr: *mut u8,
-            layout: Layout,
-        }
-
-        impl Buf {
-            fn new(len: usize) -> Self {
-                let layout = Layout::from_size_align(len, ALIGNMENT).unwrap();
-                let ptr = unsafe { alloc_zeroed(layout) };
-
-                Self { layout, ptr }
-            }
-        }
-
-        impl Drop for Buf {
-            fn drop(&mut self) {
-                unsafe {
-                    dealloc(self.ptr, self.layout);
-                }
-            }
-        }
-
         let buf = Buf::new(64);
 
         assert!(!SseFilter.insert(buf.ptr, 2, 69));
