d169fdfc9e12be4ea2afeac9419a7ee399d40d62
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2023-06-24 12:53:59 +0600

    fix warning

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -39,6 +39,11 @@ pub(crate) fn load() -> &'static dyn crate::FilterImpl {
     &fallback::FallbackFilter
 }
 
+#[cfg(any(
+    target_arch = "x86_64",
+    target_arch = "aarch64",
+    all(target_family = "wasm", target_feature = "simd128")
+))]
 const SALT: [u32; 8] = [
     0x47b6137b, 0x44974d91, 0x8824ad5b, 0xa2b7289d, 0x705495c7, 0x2df1424b, 0x9efc4947, 0x5c6bfb31,
 ];
