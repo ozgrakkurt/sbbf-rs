e9edce608f46cc8546ab56ab3682cbf30cd557cb
Özgür Akkurt <oezgurmakkurt@gmail.com>
2023-06-30 07:35:58 +0600

    refactor: use vld1q_dup_u32 in aarch64 impl, remove buildjet from CI (#7)

--- src/arch/aarch64.rs
+++ src/arch/aarch64.rs
@@ -1,6 +1,6 @@
 use super::SALT;
 use core::arch::aarch64::{
-    uint32x4_t, vandq_u32, vceqq_u32, vld1q_u32, vminvq_u32, vmulq_u32, vorrq_u32,
+    uint32x4_t, vandq_u32, vceqq_u32, vld1q_dup_u32, vld1q_u32, vminvq_u32, vmulq_u32, vorrq_u32,
     vreinterpretq_s32_u32, vshlq_u32, vshrq_n_u32, vst1q_u32,
 };
 
@@ -13,10 +13,10 @@ impl NeonFilter {
     #[inline]
     unsafe fn make_mask(hash: u32) -> (uint32x4_t, uint32x4_t) {
         let salt = (vld1q_u32(SALT.as_ptr()), vld1q_u32(SALT[4..].as_ptr()));
-        let hash = vld1q_u32([hash, hash, hash, hash].as_ptr());
+        let hash = vld1q_dup_u32(&hash);
         let mut acc = (vmulq_u32(salt.0, hash), vmulq_u32(salt.1, hash));
         acc = (vshrq_n_u32(acc.0, 27), vshrq_n_u32(acc.1, 27));
-        let ones = vld1q_u32([1, 1, 1, 1].as_ptr());
+        let ones = vld1q_dup_u32(&1);
         (
             vshlq_u32(ones, vreinterpretq_s32_u32(acc.0)),
             vshlq_u32(ones, vreinterpretq_s32_u32(acc.1)),
